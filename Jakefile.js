e437c4431fc364e67fe26c655613dcf27d7460c2
Josh Perez <josh@goatslacker.com>
2011-05-08 23:48:55 -0700

    removing bin files and replacing them with a Jakefile

---
+++ Jakefile.js
@@ -0,0 +1,32 @@
+var sys = require("sys"),
+    exec = require('child_process').exec,
+    command = "cp -R",
+    files = ['.vimrc', '.vim', '.jshint.json'],
+    target = '~/';
+
+
+function run(command_to_run, file) {
+  exec(command_to_run, function (err, std_out, std_err) {
+    if (err) {
+      console.log(file + " failed");
+    } else {
+      console.log("copied " + file + " successfully");
+    }
+  });
+}
+
+task("install", [], function () {
+   
+  files.forEach(function (file) {
+    var statement = command + " " + file + " " + target;
+    run(statement, file);
+  });
+
+}, true);
+
+task("update-git", [], function () {
+  files.forEach(function (file) {
+    var statement = command + " " + target + file + " .";
+    run(statement, file);
+  });
+});
